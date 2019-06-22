6e0c1c83ef72a1914caeb381a52647ce7a5d7bc1
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-06-22 13:26:52 +0900

    PostNewTweetを書いた

--- model/tweet.go
+++ model/tweet.go
@@ -1,10 +1,26 @@
 package model
 
+import (
+	"github.com/ChimeraCoder/anaconda"
+)
+
 // 新規ツイート用
 type NewTweet struct {
 	Text string `json:"text,omitempty"`
 }
 
-func PostNewTweet(newTweet NewTweet) {
+type StampTweet struct {
+	Tweet anaconda.Tweet
+	Stamp
+}
+
+func PostNewTweet(newTweet NewTweet) (StampTweet, error) {
 	tweet, err := api.PostTweet(newTweet.Text, nil)
+	stampTweet := StampTweet{}
+	stampTweet.Tweet = tweet
+	if err != nil {
+		return stampTweet, err
+	}
+
+	return stampTweet, nil
 }
