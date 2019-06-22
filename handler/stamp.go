4b052fd6a65f0dd5367dde314cb70280d4358a25
sola <pskt0109@docomo.ne.jp>
2019-06-22 11:03:59 +0900

    途中段階

---
+++ handler/stamp.go
@@ -0,0 +1,19 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/oribe1115/oribe-todo-list-server/model"
+)
+
+//スタンプが押されたときのhandler
+func StampAddHandler(c echo.Context) error {
+
+}
+
+//スタンプをキャンセルするときのhandler
+func StampCancelHandler(c echo.contexr) error {
+	
+}
