e62d511b9a5a013eee1404d5ad8f6b949e24634c
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-06-22 16:15:37 +0900

    GetUserTimelineを書いた

--- model/timeline.go
+++ model/timeline.go
@@ -22,3 +22,22 @@ func GetHomeTimeline(count int) ([]StampTweet, error) {
 	return stampTweetList, nil
 
 }
+
+// 変換の都合上、userIDをstringのままで
+func GetUserTimeline(userID string, count int) ([]StampTweet, error) {
+	v := url.Values{}
+	v.Set("count", strconv.Itoa(count))
+	v.Set("user_id", userID)
+
+	tweetList, err := api.GetUserTimeline(v)
+	if err != nil {
+		return nil, err
+	}
+
+	stampTweetList, err := MakeStampTweet(tweetList)
+	if err != nil {
+		return nil, err
+	}
+
+	return stampTweetList, nil
+}
